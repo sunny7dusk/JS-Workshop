8628a6383ad98e226008ed2d0659c9eeb9ccde4b
Nate <nat233947@gmail.com>
2021-10-24 12:35:15 -0400

    Final commit

--- part1/workshop.js
+++ part1/workshop.js
@@ -1,1 +1,58 @@
+// let name = "Nate";
+// let age = 20;
+// let csStudent = true;
+// let variable1 = undefined;
+// let variable = null;
+// console.log(name);
+// console.log(age);
+// console.log(csStudent);
+// console.log(variable);
 
+// let name = "Nate";
+// name = 1;
+// console.log(name);
+
+// let sentence = "My name is Nate";
+// let sentence1 = "My name is Nate";
+// let name = "Nate";
+// let sentence2 = `My name is ${name}`;
+// console.log(sentence2);
+
+// let greater = 1 === "1";
+// console.log(greater);
+
+// let isCSstudent = false;
+// isCSstudent
+//   ? console.log("Im as cs student")
+//   : console.log("Im not a cs student");
+
+// let i = 0;
+// while (i < 10) {
+//   i++;
+//   console.log(i);
+// }
+
+// for (let i = 0; i < 10; i++) {
+//   console.log(i);
+// }
+
+// let i = 2;
+// switch (i) {
+//   case 1:
+//     console.log(i);
+//     break;
+//   default:
+//     console.log("This is not 1");
+//     break;
+// }
+
+// function sum(a, b, c) {
+//   return a + b + c;
+// }
+
+// console.log(sum(3, 2, 3));
+
+let sum = function (a, b) {
+  return a + b;
+};
+console.log(sum(2, 2));
