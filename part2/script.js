54441a2ade11495a3034a366e04db76d9a6c8976
Nate <nat233947@gmail.com>
2021-10-24 15:11:58 -0400

    Made a basic notepad

--- part2/script.js
+++ part2/script.js
@@ -1,1 +1,31 @@
+let insertButton = document.querySelector("#insertText");
 
+let notesArray = [];
+
+let clearNoteContainer = function () {
+  let noteContainer = document.querySelector("#notes");
+  while (noteContainer.firstChild) {
+    noteContainer.removeChild(noteContainer.firstChild);
+  }
+};
+
+insertButton.addEventListener("click", () => {
+  let noteText = document.querySelector("#noteText").value;
+
+  notesArray.push(noteText);
+
+  clearNoteContainer();
+
+  notesArray.forEach((value) => {
+    let noteDiv = document.createElement("div");
+    noteDiv.classList.add("note");
+    let noteTextElement = document.createElement("h3");
+    let noteDelete = document.createElement("button");
+
+    noteTextElement.appendChild(document.createTextNode(`${value}`));
+    noteDiv.appendChild(noteTextElement);
+
+    let noteContainer = document.querySelector("#notes");
+    noteContainer.prepend(noteDiv);
+  });
+});
